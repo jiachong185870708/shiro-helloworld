6e15e01cb1576765a328eb9df134fbcdfd628c3c
jiachong <185870708@qq.com>
2016-08-01 21:38:25 +0800

    CCC
    CCCC

--- shiro-helloworld/src/com/atguigu/shiro/helloworld/HelloWorld.java
+++ shiro-helloworld/src/com/atguigu/shiro/helloworld/HelloWorld.java
@@ -5,5 +5,5 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("AAAAAAAAA");
 		System.out.println("BBBBBBBBB");
-	}
+		System.out.println("CCCCCCCCC");
 }
