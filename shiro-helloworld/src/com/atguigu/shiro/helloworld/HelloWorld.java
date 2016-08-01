506f22ee314725cdaf74071204056b522b316fbd
jiachong185870708 <185870708@qq.com>
2016-08-01 21:38:38 +0800

    DDDD

--- shiro-helloworld/src/com/atguigu/shiro/helloworld/HelloWorld.java
+++ shiro-helloworld/src/com/atguigu/shiro/helloworld/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("AAAAAAAAA");
 		System.out.println("BBBBBBBBB");
+		System.out.println("DDDDDDDDD");
+		
 	}
 }
